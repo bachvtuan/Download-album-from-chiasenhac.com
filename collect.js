b5a87d1a4f4a2f63b40bc31a86905db681740385
Tuan Bach Van <bachvtuan@gmail.com>
2014-01-04 21:43:43 +0700

    initialize code

---
+++ collect.js
@@ -0,0 +1,89 @@
+var x = require('casper').selectXPath;
+var download_links = [];
+var fs = require('fs');
+
+var support_qualities = ['128kbps','320kbps','500kbps','Lossless','32kbps'];
+
+var result = {
+  '128kbps': [],
+  '320kbps': [],
+  '500kbps': [],
+  'Lossless': [],
+  '32kbps': []
+};
+
+// --disk-cache=true
+
+var casper = require('casper').create({
+  pageSettings: {
+        loadImages:  false,        // do not load images
+        loadPlugins: false         // do not load NPAPI plugins (Flash, Silverlight, ...)
+    }
+});
+
+if (casper.cli.args.length != 3){
+  casper.echo("Wrong syntex casperjs collect.js {album_url} {username} {password}")
+  phantom.exit()
+}
+
+//Example url:http://playlist.chiasenhac.com/nghe-album/surrender~omar-akram~1015073.html
+var album_url = casper.cli.args[0]; 
+var username = casper.cli.args[1];
+var password = casper.cli.args[2];
+
+casper.echo( "Download album :" +album_url + " with username="+ username +" and password="+password );
+
+casper.echo("Authentication user");
+
+casper.start('http://chiasenhac.com/login.php',function(){
+ this.evaluate(function(username,password){
+    	document.getElementsByName("username")[0].value = username;
+    	document.getElementsByName("password")[0].value = password;
+    },username,password);	
+  this.click('input[name="login"]');
+
+  casper.wait(5000, function() {
+     this.echo('redirect to album page');
+  });
+});
+
+
+casper.thenOpen(album_url, function() {
+  listen_links = this.getElementsAttribute('div.playlist_prv tr td span a.musictitle','href');
+  casper.echo(listen_links.length);
+  casper.echo(listen_links);
+
+  download_links = this.getElementsAttribute('div.playlist_prv tr td span a:first-child','href');
+  casper.echo(download_links.length);
+  casper.echo(download_links);
+
+});
+
+var index = -1;
+casper.then(function() {
+  this.eachThen(download_links, function() { 
+      index++; 
+      casper.echo("Opening "+ download_links[index]);
+      this.thenOpen((download_links[index]), function() {
+        this.echo(this.getTitle()); // display the title of page
+        file_urls = this.getElementsAttribute('div#downloadlink b a','href');
+        for (var i = 0; i < file_urls.length ; i++){
+          for (var j = 0; j < support_qualities.length;j++){
+            if (file_urls[i].indexOf(support_qualities[j]) != -1){
+              result[support_qualities[j]].push(file_urls[i]);
+            }
+          }
+        }
+        
+      });
+  });
+});
+
+casper.then(function(){
+  log("Done, writing to file");
+  fs.write('data.txt', JSON.stringify(result), 'w');
+});
+
+casper.run();
+
+//Note: You can use  this.download("{url}", "{file_name}") instead using python to download file
---
+++ download.py
@@ -0,0 +1,103 @@
+import urllib2,sys,json,os
+
+def chunk_report(bytes_so_far, total_size,mb_size):
+  percent = float(bytes_so_far) / total_size
+  percent = round(percent*100, 2)
+  sys.stdout.write("Downloaded %d of %0.2f bytes (%.3f MB) (%0.2f%%)\r" % 
+    (bytes_so_far, total_size, mb_size, percent))
+
+  if bytes_so_far >= total_size:
+    sys.stdout.write('\n')
+
+def chunk_read(response, chunk_size=8192, report_hook=None):
+  total_size = response.info().getheader('Content-Length').strip()
+  total_size = int(total_size)
+  mb_size = total_size/float(1024*1024)
+  
+  bytes_so_far = 0
+  data = []
+
+  while 1:
+    chunk = response.read(chunk_size)
+    bytes_so_far += len(chunk)
+
+    if not chunk:
+      break
+    data += chunk
+    if report_hook:
+      report_hook(bytes_so_far, total_size,mb_size)
+
+  return "".join(data)
+
+if __name__ == '__main__':
+
+  print sys.argv
+  if len(sys.argv) != 3:
+    print "wrong argv python download.py quality(32,128,320,500,lossless) folder_save"
+    sys.exit()
+
+  quality = sys.argv[1]
+  folder = sys.argv[2]
+
+  assert quality in ['32','128','320','500','lossless']
+  
+  mapping = {
+    '32' : {
+      'full': '32kbps',
+      'find' : ' [M4A 32kbps]'
+    },
+    '128' : {
+      'full': '128kbps',
+      'find' : ' [MP3 128kbps]'
+    },
+    '320' : {
+      'full': '320kbps',
+      'find' : ' [MP3 320kbps]'
+    },
+    '500' : {
+      'full': '500kbps',
+      'find' : ' [M4A 500kbps]'
+    },
+    'lossless' : {
+      'full': 'Lossless',
+      'find' : ' [FLAC Lossless]'
+    },
+  }
+
+  file = open('data.txt', 'r')
+  files = json.loads(file.read())
+  
+  map_item  = mapping[quality]
+
+  total_files = len(files[ map_item['full'] ])
+  index = 1;
+
+  #Create folder if folder doesn't exist
+  if not os.path.exists(folder):
+    os.makedirs(folder)
+
+  for file_url in files[ map_item['full'] ]:
+    file_name = os.path.basename(file_url)
+
+    #1. First song.flac
+    file_name =  str(index) + ". " + urllib2.unquote(file_name)
+
+    #Remove uncessary string at the end of file
+    file_name = file_name.replace( map_item['find'],"")
+    
+    file_name =  folder + "/" + file_name
+
+    print "Downloading file_name {0}/{1} files ".format(index, total_files)
+
+    #Get file content
+    response = urllib2.urlopen(file_url)
+    #Create file with write mode
+    output = open(file_name, "w")
+    #Write request content to file
+    output.write(chunk_read(response, report_hook=chunk_report))
+    #Close file
+    output.close()
+
+    index += 1
+    
+  print "Download done, have fun :)"
